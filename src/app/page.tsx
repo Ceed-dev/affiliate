d77977abc50546de899d98b6459d0da549cb1d6f
shungo0222 <kimura.shungo@gmail.com>
2024-04-03 15:45:06 +0900

    Create an onboarding page
    Related to #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-slate-200">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
         <title>Simple Landing Page</title>
         <link rel="icon" href="/favicon.ico" />
