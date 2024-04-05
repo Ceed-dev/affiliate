55c055236f025e428aedef47014c8afdee9d2aa4
shungo0222 <kimura.shungo@gmail.com>
2024-04-05 13:28:56 +0900

    Create a project overview page
    Related to #7

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+
 export default function Projects() {
   return (
-    <p>projects</p>
+    <div className="w-2/3 mx-auto mt-10">
+      <div className="flex flex-row justify-between items-center mb-10">
+        <h1 className="text-2xl font-semibold">Your Projects</h1>
+        <Link 
+          href="/create-project" 
+          className="bg-sky-500 text-white py-2 px-3 text-sm rounded-md transition duration-300 ease-in-out transform hover:scale-105"
+        >
+          + New Project
+        </Link>
+      </div>
+      <div className="text-center">
+        <p className="text-sm">No projects</p>
+        <p className="text-sm text-gray-500">Get started by creating a new project.</p>
+      </div>
+    </div>
   );
 }
