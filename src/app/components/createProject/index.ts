ca245e19ca796e2382f2b1d8bc54ed1793c083bd
shungo0222 <kimura.shungo@gmail.com>
2024-04-08 17:32:02 +0900

    Create `SocialLinksForm` component
    Related to #23

--- src/app/components/createProject/index.ts
+++ src/app/components/createProject/index.ts
@@ -1,9 +1,11 @@
 import { ProjectDetailsForm } from "./ProjectDetailsForm";
 import { AffiliatesForm } from "./AffiliatesForm";
 import { LogoForm } from "./LogoForm";
+import { SocialLinksForm } from "./SocialLinksForm";
 
 export {
   ProjectDetailsForm,
   AffiliatesForm,
-  LogoForm
+  LogoForm,
+  SocialLinksForm
 }
--- src/app/projects/create-project/page.tsx
+++ src/app/projects/create-project/page.tsx
@@ -6,7 +6,8 @@ import { StatusBarComponent } from "../../components/statusbar/StatusBarComponen
 import { 
   ProjectDetailsForm,
   AffiliatesForm,
-  LogoForm
+  LogoForm,
+  SocialLinksForm
 } from "../../components/createProject";
 
 export default function CreateProject() {
@@ -52,91 +53,9 @@ export default function CreateProject() {
       ...prev,
       [field]: event.target.value
     }));
-    console.log("projectData: ", projectData);
   };
 
-  const [currentStep, setCurrentStep] = useState(3);
-
-  const [step, setStep] = useState(1);
-  const [formTitle, setFormTitle] = useState("Project Details");
-  const [projectName, setProjectName] = useState("");
-  const [slug, setSlug] = useState("my-project");
-  const [description, setDescription] = useState("");
-
-  const [selectedToken, setSelectedToken] = useState("");
-  const [rewardAmount, setRewardAmount] = useState("");
-  const [redirectUrl, setRedirectUrl] = useState("");
-
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-
-    if (step === 3) {
-      window.open("http://localhost:3000/test-nft-collection");
-    } else {
-      setStep((prevStep) => {
-        if (prevStep === 3) {
-          return 1;
-        } else {
-          return prevStep + 1;
-        }
-      });
-      setFormTitle((prevFormTitle) => {
-        if (prevFormTitle === "Project Details") {
-          return "Affiliates";
-        } else if (prevFormTitle === "Affiliates") {
-          return "You've created a project!!";
-        } else {
-          return "Project Details";
-        }
-      });
-    }
-  };
-
-  const handleDeposit = () => {
-    console.log("Deposit action initiated");
-  };
-
-  const [logo, setLogo] = useState(null);
-  const [logoPreview, setLogoPreview] = useState("");
-  const [coverImage, setCoverImage] = useState(null);
-  const [coverPreview, setCoverPreview] = useState("");
-
-  const handleLogoChange = (event: any) => {
-    const file = event.target.files[0];
-    if (file) {
-      setLogo(file);
-      setLogoPreview(URL.createObjectURL(file));
-    }
-  };
-
-  const removeLogoImage = () => {
-    if (logoPreview) {
-      URL.revokeObjectURL(logoPreview);
-    }
-    setLogo(null);
-    setLogoPreview("");
-  }
-
-  const handleCoverChange = (event: any) => {
-    const file = event.target.files[0];
-    if (file) {
-      setCoverImage(file);
-      setCoverPreview(URL.createObjectURL(file));
-    }
-  };
-
-  const removeCoverImage = () => {
-    if (coverPreview) {
-      URL.revokeObjectURL(coverPreview);
-    }
-    setCoverImage(null);
-    setCoverPreview("");
-  }
-
-  const [websiteUrl, setWebsiteUrl] = useState("");
-  const [discordUrl, setDiscordUrl] = useState("");
-  const [twitterUrl, setTwitterUrl] = useState("");
-  const [instagramUrl, setInstagramUrl] = useState("");
+  const [currentStep, setCurrentStep] = useState(4);
 
   return (
     <div className="flex flex-col">
@@ -176,59 +95,17 @@ export default function CreateProject() {
         />
       }
 
-      {currentStep === 4 && <div className="bg-white w-2/5 rounded-lg shadow-md p-5 mx-auto mt-10 text-sm">
-
-        <h1 className="text-xl mb-5">Socials</h1>
-
-        <div className="flex flex-col gap-5">
-
-          <div className="flex flex-col gap-2">
-            <h2>Website</h2>
-            <input
-              type="url"
-              placeholder="https://mysite.com"
-              value={websiteUrl}
-              onChange={(e) => setWebsiteUrl(e.target.value)}
-              className="w-full p-2 border border-[#D1D5DB] rounded-lg text-sm outline-none"
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h2>Discord</h2>
-            <input
-              type="url"
-              placeholder="https://discord.gg/my-group"
-              value={discordUrl}
-              onChange={(e) => setDiscordUrl(e.target.value)}
-              className="w-full p-2 border border-[#D1D5DB] rounded-lg text-sm outline-none"
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h2>Twitter</h2>
-            <input
-              type="url"
-              placeholder="https://twitter.com/my-twitter"
-              value={twitterUrl}
-              onChange={(e) => setTwitterUrl(e.target.value)}
-              className="w-full p-2 border border-[#D1D5DB] rounded-lg text-sm outline-none"
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h2>Instagram</h2>
-            <input
-              type="url"
-              placeholder="https://instagram.com/my-insta"
-              value={instagramUrl}
-              onChange={(e) => setInstagramUrl(e.target.value)}
-              className="w-full p-2 border border-[#D1D5DB] rounded-lg text-sm outline-none"
-            />
-          </div>
-
-        </div>
-
-      </div>}
+      {currentStep === 4 &&
+        <SocialLinksForm
+          data={{
+            websiteUrl: projectData.websiteUrl,
+            discordUrl: projectData.discordUrl,
+            twitterUrl: projectData.twitterUrl,
+            instagramUrl: projectData.instagramUrl
+          }}
+          handleChange={handleChange}
+        />
+      }
 
     </div>
   );
