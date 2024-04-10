3eb26fab3b5f9e8d01ba711e42c559fd46d69e41
shungo0222 <kimura.shungo@gmail.com>
2024-04-10 16:43:08 +0900

    Create image type file
    Related to #62

--- src/app/utils/firebase/uploadImageAndGetURL.ts
+++ src/app/utils/firebase/uploadImageAndGetURL.ts
@@ -1,6 +1,8 @@
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
-export const uploadImageAndGetURL = async (file: any, projectId: string, imageType: string): Promise<string | null> => {
+import { ImageType } from "../../types";
+
+export const uploadImageAndGetURL = async (file: any, projectId: string, imageType: ImageType): Promise<string | null> => {
   if (!file) return null;
   const storage = getStorage();
   const storageRef = ref(storage, `projectImages/${projectId}/${imageType}_${file.name}`);
