539f2dd16c4c83e0133c6fb4b340ded44ffce485
shungo0222 <kimura.shungo@gmail.com>
2024-04-15 14:05:42 +0900

    Create a type file for user data
    Related to #113

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,9 +1,11 @@
 import { ProjectData } from "./projectData";
 import { ImageType } from "./imageType";
 import { ReferralData } from "./referralData";
+import { UserData } from "./userData";
 
 export type {
   ProjectData,
   ImageType,
-  ReferralData
+  ReferralData,
+  UserData
 }
