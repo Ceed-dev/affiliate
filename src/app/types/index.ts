6d3a89d8308892ba178a41e41de18b246df4bc7e
shungo0222 <kimura.shungo@gmail.com>
2024-04-15 13:24:47 +0900

    Create a type file of referral data
    Related to #111

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,7 +1,9 @@
 import { ProjectData } from "./projectData";
 import { ImageType } from "./imageType";
+import { ReferralData } from "./referralData";
 
 export type {
   ProjectData,
-  ImageType
+  ImageType,
+  ReferralData
 }
