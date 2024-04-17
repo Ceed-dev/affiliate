60d60a7aa158ba7056bcabc617b07400222286d9
shungo0222 <kimura.shungo@gmail.com>
2024-04-17 12:07:12 +0900

    Fetch project and referral data after api calling
    Related to #140

--- src/app/api/conversion/route.ts
+++ src/app/api/conversion/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { fetchProjectData, fetchReferralData } from "../../utils/firebase";
 
 export async function POST(request: NextRequest) {
   try {
@@ -11,7 +12,25 @@ export async function POST(request: NextRequest) {
       );
     }
 
-    // ここでコンバージョンを確認し、成功した場合は適切な処理を行う
+    // リファラルデータを取得
+    const referralData = await fetchReferralData(referral);
+
+    if (!referralData) {
+      return NextResponse.json(
+        { error: "Referral data not found" },
+        { status: 404 }
+      );
+    }
+
+    // プロジェクトデータを取得
+    const projectData = await fetchProjectData(referralData.projectId);
+
+    if (!projectData) {
+      return NextResponse.json(
+        { error: "Project data not found" },
+        { status: 404 }
+      );
+    }
 
     // 成功した場合はリクエストが正常に処理されたことを返す
     return NextResponse.json(
--- src/app/utils/firebase/index.ts
+++ src/app/utils/firebase/index.ts
@@ -1,11 +1,13 @@
 import { saveProjectToFirestore } from "./saveProjectToFirestore";
 import { fetchProjectData } from "./fetchProjectData";
 import { fetchAllProjects } from "./fetchAllProjects";
+import { fetchReferralData } from "./fetchReferralData";
 import { joinProject } from "./joinProject";
 
 export {
   saveProjectToFirestore,
   fetchProjectData,
   fetchAllProjects,
+  fetchReferralData,
   joinProject
 }
