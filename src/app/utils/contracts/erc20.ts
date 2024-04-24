aa1e53898fbf5374c394c4a689944bd2227beeb1
shungo0222 <kimura.shungo@gmail.com>
2024-04-24 10:52:03 +0900

    Add try-catch to ERC20 Class
    Related to #194

--- src/app/utils/contracts/erc20.ts
+++ src/app/utils/contracts/erc20.ts
@@ -12,30 +12,55 @@ export class ERC20 {
   }
 
   async getBalance(address: string): Promise<string> {
-    const decimals = await this.getDecimals();
-    const balance = await this.contract.balanceOf(address);
-    return ethers.utils.formatUnits(balance, decimals);
+    try {
+      const decimals = await this.getDecimals();
+      const balance = await this.contract.balanceOf(address);
+      return ethers.utils.formatUnits(balance, decimals);
+    } catch (error: any) {
+      console.error("Failed to retrieve balance:", error);
+      throw new Error(`Failed to retrieve balance: ${error.message}`);
+    }
   }
 
   async getDecimals(): Promise<number> {
-    return await this.contract.decimals();
+    try {
+      return await this.contract.decimals();
+    } catch (error: any) {
+      console.error("Failed to retrieve decimals:", error);
+      throw new Error(`Failed to retrieve decimals: ${error.message}`);
+    }
   }
 
   async getSymbol(): Promise<string> {
-    return await this.contract.symbol();
+    try {
+      return await this.contract.symbol();
+    } catch (error: any) {
+      console.error("Failed to retrieve symbol:", error);
+      throw new Error(`Failed to retrieve symbol: ${error.message}`);
+    }
   }
 
   async getAllowance(owner: string, spender: string): Promise<string> {
-    const decimals = await this.getDecimals();
-    const allowance = await this.contract.allowance(owner, spender);
-    return ethers.utils.formatUnits(allowance, decimals);
+    try {
+      const decimals = await this.getDecimals();
+      const allowance = await this.contract.allowance(owner, spender);
+      return ethers.utils.formatUnits(allowance, decimals);
+    } catch (error: any) {
+      console.error("Failed to retrieve allowance:", error);
+      throw new Error(`Failed to retrieve allowance: ${error.message}`);
+    }
   }
 
   async approve(spender: string, amount: number): Promise<string> {
-    const decimals = await this.getDecimals();
-    const formattedAmount = ethers.utils.parseUnits(amount.toString(), decimals);
-    const transactionResponse = await this.contract.approve(spender, formattedAmount);
-    await transactionResponse.wait();
-    return transactionResponse.hash;
+    try {
+      const decimals = await this.getDecimals();
+      const formattedAmount = ethers.utils.parseUnits(amount.toString(), decimals);
+      const transactionResponse = await this.contract.approve(spender, formattedAmount);
+      await transactionResponse.wait();
+      return transactionResponse.hash;
+    } catch (error: any) {
+      console.error("Approval failed:", error);
+      throw new Error(`Approval failed: ${error.message}`);
+    }
   }
 }
