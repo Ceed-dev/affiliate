4eb171eb8d2e4a1f59523134609375b088a7c3f2
shungo0222 <kimura.shungo@gmail.com>
2024-04-24 10:05:57 +0900

    Add try-catch to Escrow Class
    Related to #191

--- src/app/utils/contracts/escrow.ts
+++ src/app/utils/contracts/escrow.ts
@@ -5,24 +5,34 @@ export class Escrow {
   private contract: ethers.Contract;
 
   constructor(signer: ethers.Signer) {
-    this.contract = new ethers.Contract(
-      `${process.env.ESCROW_CONTRACT_ADDRESS}`, 
-      escrowABI, 
-      signer
-    );
+    const contractAddress = `${process.env.ESCROW_CONTRACT_ADDRESS}`;
+    if (!ethers.utils.isAddress(contractAddress)) {
+      throw new Error("Invalid contract address.");
+    }
+    this.contract = new ethers.Contract(contractAddress, escrowABI, signer);
   }
 
   async deposit(tokenAddress: string, amount: number, decimals: number): Promise<string> {
-    const formattedAmount = ethers.utils.parseUnits(amount.toString(), decimals);
-    const transactionResponse = await this.contract.deposit(tokenAddress, formattedAmount);
-    await transactionResponse.wait();
-    return transactionResponse.hash;
+    try {
+      const formattedAmount = ethers.utils.parseUnits(amount.toString(), decimals);
+      const transactionResponse = await this.contract.deposit(tokenAddress, formattedAmount);
+      await transactionResponse.wait();
+      return transactionResponse.hash;
+    } catch (error) {
+      console.error("Failed to deposit:", error);
+      throw error;
+    }
   }
 
   async withdraw(tokenAddress: string, amount: number, decimals: number, depositorAddress: string, recipientAddress: string): Promise<string> {
-    const formattedAmount = ethers.utils.parseUnits(amount.toString(), decimals);
-    const transactionResponse = await this.contract.withdraw(tokenAddress, formattedAmount, depositorAddress, recipientAddress);
-    await transactionResponse.wait();
-    return transactionResponse.hash;
+    try {
+      const formattedAmount = ethers.utils.parseUnits(amount.toString(), decimals);
+      const transactionResponse = await this.contract.withdraw(tokenAddress, formattedAmount, depositorAddress, recipientAddress);
+      await transactionResponse.wait();
+      return transactionResponse.hash;
+    } catch (error) {
+      console.error("Failed to withdraw:", error);
+      throw error;
+    }
   }
 }
