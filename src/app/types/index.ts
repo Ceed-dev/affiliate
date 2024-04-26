7c846c2a05d298dd73260ff3df93566cb73d8532
shungo0222 <kimura.shungo@gmail.com>
2024-04-26 12:38:31 +0900

    Create "PaymentTransaction" type
    Related to #221

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -2,10 +2,12 @@ import { ProjectData } from "./projectData";
 import { ImageType } from "./imageType";
 import { ReferralData } from "./referralData";
 import { UserData } from "./userData";
+import { PaymentTransaction } from "./paymentTransaction";
 
 export type {
   ProjectData,
   ImageType,
   ReferralData,
-  UserData
+  UserData,
+  PaymentTransaction
 }
