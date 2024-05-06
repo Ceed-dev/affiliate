372c17aaad16b75a2402aaa2e70f285c73bff881
shungo0222 <kimura.shungo@gmail.com>
2024-05-06 12:22:29 +0900

    Adapt AffiliatesForm for Editing and Integrate into Settings Page
    Related to #280

--- src/app/projects/[projectId]/settings/page.tsx
+++ src/app/projects/[projectId]/settings/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import Image from "next/image";
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import { ProjectData } from "../../../types";
 import { NavBar } from "../../../components/dashboard";
-import { ProjectDetailsForm, NextButton } from "../../../components/createProject";
+import { 
+  ProjectDetailsForm, 
+  AffiliatesForm,
+  NextButton 
+} from "../../../components/createProject";
 import { fetchProjectData } from "../../../utils/firebase";
 
 export default function Settings({ params }: { params: { projectId: string } }) {
@@ -47,7 +52,10 @@ export default function Settings({ params }: { params: { projectId: string } }) 
   const isFormComplete = () => {
     return projectData &&
       projectData.projectName.trim() !== "" &&
-      projectData.description.trim() !== ""
+      projectData.description.trim() !== "" &&
+      projectData.selectedTokenAddress.trim() !== "" &&
+      projectData.rewardAmount > 0 &&
+      projectData.redirectUrl.trim() !== ""
   };
 
   const handleSaveChanges = () => {
@@ -65,17 +73,35 @@ export default function Settings({ params }: { params: { projectId: string } }) 
     <>
       <NavBar projectId={params.projectId} />
       <div className="min-h-screen bg-[#F8FAFC] px-4 sm:px-10 md:px-32 lg:px-60 xl:px-96 pt-1 pb-10">
-        <ProjectDetailsForm
-          data={{
-            projectName: `${projectData?.projectName}`,
-            description: `${projectData?.description}`
-          }}
-          handleChange={handleChange}
-          isEditing={true}
-        />
-        <NextButton onClick={handleSaveChanges} disabled={!isFormComplete() || !hasChanges()}>
-          Save Changes
-        </NextButton>
+        {loadingProject ? (
+          <div className="flex flex-row items-center justify-center gap-5 mt-20">
+            <Image src="/loading.png" alt="loading.png" width={50} height={50} className="animate-spin" /> 
+            <p className="text-gray-500 font-semibold text-lg">Loading...</p>
+          </div>
+        ) : (
+          <>
+            <ProjectDetailsForm
+              data={{
+                projectName: `${projectData?.projectName}`,
+                description: `${projectData?.description}`
+              }}
+              handleChange={handleChange}
+              isEditing={true}
+            />
+            <AffiliatesForm 
+              data={{
+                selectedTokenAddress: `${projectData?.selectedTokenAddress}`,
+                rewardAmount: projectData?.rewardAmount ?? 0,
+                redirectUrl: `${projectData?.redirectUrl}`
+              }}
+              handleChange={handleChange}
+              isEditing={true}
+            />
+            <NextButton onClick={handleSaveChanges} disabled={!isFormComplete() || !hasChanges()}>
+              Save Changes
+            </NextButton>
+          </>
+        )}
       </div>
     </>
   );
