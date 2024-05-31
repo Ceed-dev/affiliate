8675c691a5eec56f79ccc55ead146fd460c54072
shungo0222 <kimura.shungo@gmail.com>
2024-05-31 11:23:10 +0900

    Modify Project Detail Page Code for Navigation from Marketplace
    Related to #368

--- src/app/affiliate/[projectId]/page.tsx
+++ src/app/affiliate/[projectId]/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import { ConnectWallet, lightTheme, useAddress, WalletInstance, useDisconnect } from "@thirdweb-dev/react";
 import { toast } from "react-toastify";
-import { ProjectData, ReferralData, PaymentTransaction } from "../../types";
+import { ProjectData, DirectPaymentProjectData, ReferralData, PaymentTransaction } from "../../types";
 import { ProjectHeader, ConversionsList } from "../../components/affiliate";
 import { StatisticCard } from "../../components/dashboard/StatisticCard";
 import { fetchProjectData, fetchReferralData, joinProject, fetchTransactionsForReferrals } from "../../utils/firebase";
@@ -31,70 +31,43 @@ export default function Affiliate({ params }: { params: { projectId: string } })
   const [tokenSymbol, setTokenSymbol] = useState("");
   const [loadingTokenSymbol, setLoadingTokenSymbol] = useState(true);
 
-  const countdown = useCountdown(projectData?.deadline ?? undefined);
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const [referralLink, setReferralLink] = useState("");
 
-  // TODO: Google Formリンク表示機能を一時的に追加。リファラルID機能をコメントアウト。
-  // リダイレクトリンク変数をGoogle Formリンクを保持するための変数として使用。
-  // しかし、将来的にはそれぞれのギルドのウォレットアドレスに対して別々のGoogle Formリンクを
-  // 表示する必要があるため、修正が必要。
-  // - Reason: ギルド向け機能の開発のため。
-  // - Planned Reversion: 未定。
-  // - Date: 2024-05-15
-  // - Author: shungo0222
-  // - Issue: #304
-  // ===== UPDATE =====
-  // - Additional Modification: ウォレット接続時に該当のURLを設定する。
-  // - Reason for Update: 接続したユーザーがコピーできるようにするため。
-  // - Date of Update: 2024-05-17
-  // - Author: shungo0222
-  // - Issue: #315
-  // ===== BEGIN ORIGINAL CODE =====
-  // const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  // const REFERRAL_LINK = `${baseUrl}/referee/${params.projectId}/${referralId}`;
-  // ===== END ORIGINAL CODE =====
-  // ===== BEGIN MODIFICATION =====
-  const [REFERRAL_LINK, SET_REFERRAL_LINK] = useState("");
-  useEffect(() => {
-    if (address && projectData?.whitelistedAddresses[address]) {
-      // If an address exists in the whitelist, set the redirect URL for that address.
-      SET_REFERRAL_LINK(projectData.whitelistedAddresses[address].redirectUrl);
-    } else {
-      // Reset link if address is not in whitelist
-      SET_REFERRAL_LINK("");
-    }
-  }, [address, projectData?.whitelistedAddresses]);
-  // ===== END MODIFICATION =====
-
-  // TODO: ウォレットがホワイトリスト化されているかどうか、報酬に関するテキストの表示を設定。
-  // - Reason: ウォレットごとに表示内容が異なるため。
-  // - Planned Reversion: 未定。
-  // - Date: 2024-05-17
-  // - Author: shungo0222
-  // - Issue: #315
-  // ===== BEGIN MODIFICATION =====
   const [isWhitelisted, setIsWhitelisted] = useState(false);
 
-  useEffect(() => {
-    if (address && projectData?.whitelistedAddresses[address]) {
-      setIsWhitelisted(true);
-    } else {
-      setIsWhitelisted(false);
-    }
-  }, [address, projectData?.whitelistedAddresses]);
-
-  const rewardText = loadingTokenSymbol 
-    ? <span className="text-gray-500">Loading...</span> 
-    : (
-        <span className="font-semibold bg-green-200 px-2 py-1 rounded-md shadow-lg">
-          {isWhitelisted && address && projectData?.whitelistedAddresses[address].rewardAmount} {tokenSymbol}
-        </span>
-      );
-  // ===== END MODIFICATION =====
+  const countdown = useCountdown(
+    projectData?.projectType === "DirectPayment"
+      ? (projectData as DirectPaymentProjectData).deadline ?? undefined
+      : undefined
+  );
 
   // Automatically disconnect the wallet when the page loads to ensure a clean state for session management.
   useEffect(() => {
     disconnect();
   }, []);
+
+  useEffect(() => {
+    if (projectData?.projectType === "DirectPayment") {
+      if (address && (projectData as DirectPaymentProjectData)?.whitelistedAddresses[address]) {
+        // If an address exists in the whitelist, set the redirect URL for that address.
+        setReferralLink((projectData as DirectPaymentProjectData).whitelistedAddresses[address].redirectUrl);
+      } else {
+        // Reset link if address is not in whitelist
+        setReferralLink("");
+      }
+    } else if (projectData?.projectType === "EscrowPayment") {
+      setReferralLink(`${baseUrl}/referee/${params.projectId}/${referralId}`);
+    }
+  }, [address, (projectData as DirectPaymentProjectData)?.whitelistedAddresses, projectData?.projectType]);
+
+  useEffect(() => {
+    if (projectData?.projectType === "DirectPayment" && address && (projectData as DirectPaymentProjectData).whitelistedAddresses[address]) {
+      setIsWhitelisted(true);
+    } else {
+      setIsWhitelisted(false);
+    }
+  }, [address, projectData]);
 
   useEffect(() => {
     fetchProjectData(params.projectId)
@@ -131,55 +104,52 @@ export default function Affiliate({ params }: { params: { projectId: string } })
     fetchTokenDetails();
   }, [projectData]);
 
-  // TODO: コンバージョンダッシュボードを一時的に非表示するため、リファラルデータを読み込む必要なし。
-  // - Reason: コンバージョンに関してはGoogle Sheetsなどで管理できるため。
-  // - Planned Reversion: 未定。
-  // - Date: 2024-05-15
-  // - Author: shungo0222
-  // - Issue: #306
-  // ===== BEGIN ORIGINAL CODE =====
-  // useEffect(() => {
-  //   if (referralId) {
-  //     fetchReferralData(referralId)
-  //       .then(data => {
-  //         setReferralData(data);
-  //         setLoadingReferral(false);
-  //       })
-  //       .catch(error => {
-  //         const message = (error instanceof Error) ? error.message : "Unknown error";
-  //         console.error("Error loading the referral: ", message);
-  //         toast.error(`Error loading the referral: ${message}`);
-  //         setLoadingReferral(false);
-  //       });
-  //   }
-  // }, [referralId]);
-  // ===== END ORIGINAL CODE =====
+  useEffect(() => {
+    if (referralId) {
+      fetchReferralData(referralId)
+        .then(data => {
+          setReferralData(data);
+          setLoadingReferral(false);
+        })
+        .catch(error => {
+          const message = (error instanceof Error) ? error.message : "Unknown error";
+          console.error("Error loading the referral: ", message);
+          toast.error(`Error loading the referral: ${message}`);
+          setLoadingReferral(false);
+        });
+    }
+  }, [referralId]);
 
-  // TODO: コンバージョンダッシュボードを一時的に非表示するため、トランザクションデータを読み込む必要なし。
-  // - Reason: コンバージョンに関してはGoogle Sheetsなどで管理できるため。
-  // - Planned Reversion: 未定。
-  // - Date: 2024-05-15
-  // - Author: shungo0222
-  // - Issue: #306
-  // ===== BEGIN ORIGINAL CODE =====
-  // useEffect(() => {
-  //   if (referralData) {
-  //     fetchTransactionsForReferrals([referralData], setTransactionData)
-  //       .then(() => {
-  //         setLoadingTransactionData(false);
-  //       })
-  //       .catch(error => {
-  //         console.error("Error fetching transactions: ", error.message);
-  //         toast.error(`Error fetching transactions: ${error.message}`);
-  //         setLoadingTransactionData(false);
-  //       });
-  //   }
-  // }, [referralData]);
-  // ===== END ORIGINAL CODE =====
+  useEffect(() => {
+    if (referralData) {
+      fetchTransactionsForReferrals([referralData], setTransactionData)
+        .then(() => {
+          setLoadingTransactionData(false);
+        })
+        .catch(error => {
+          console.error("Error fetching transactions: ", error.message);
+          toast.error(`Error fetching transactions: ${error.message}`);
+          setLoadingTransactionData(false);
+        });
+    }
+  }, [referralData]);
 
+  const rewardText = loadingTokenSymbol 
+    ? <span className="text-gray-500">Loading...</span> 
+    : (
+        <span className="font-semibold bg-green-200 px-2 py-1 rounded-md shadow-lg">
+          {projectData?.projectType === "DirectPayment" && isWhitelisted && address
+            ? (projectData as DirectPaymentProjectData).whitelistedAddresses[address].rewardAmount
+            : projectData?.projectType === "EscrowPayment"
+            ? projectData.rewardAmount
+            : null
+          } {tokenSymbol}
+        </span>
+      );
+  
   const copyLinkToClipboard = async () => {
     try {
-      await navigator.clipboard.writeText(REFERRAL_LINK);
+      await navigator.clipboard.writeText(referralLink);
       setButtonLabel("Copied!");
       toast.info("Link copied to clipboard!");
       setTimeout(() => setButtonLabel("Copy"), 2000);
@@ -196,26 +166,28 @@ export default function Affiliate({ params }: { params: { projectId: string } })
       <ProjectHeader projectData={projectData} loading={loadingProject} />
 
       {/* Project Status Overview */}
-      <div className="w-2/3 mx-auto grid grid-cols-1 lg:grid-cols-3 gap-5 mb-10">
-        <StatisticCard
-          title="Remaining Duration"
-          loading={loadingProject}
-          value={countdown || "Calculating time left..."}
-          unit={`Until ${displayFormattedDateWithTimeZone(projectData?.deadline ?? undefined)}`}
-        />
-        <StatisticCard
-          title="Remaining Slots"
-          loading={loadingProject}
-          value={`${projectData?.slots.remaining}/${projectData?.slots.total}`}
-          unit="Slots"
-        />
-        <StatisticCard
-          title="Budget Balance"
-          loading={loadingProject || loadingTokenSymbol}
-          value={`${projectData?.budget.remaining}/${projectData?.budget.total}`}
-          unit={tokenSymbol}
-        />
-      </div>
+      {projectData?.projectType === "DirectPayment" && (
+        <div className="w-2/3 mx-auto grid grid-cols-1 lg:grid-cols-3 gap-5 mb-10">
+          <StatisticCard
+            title="Remaining Duration"
+            loading={loadingProject}
+            value={countdown || "Calculating time left..."}
+            unit={`Until ${displayFormattedDateWithTimeZone(projectData?.deadline ?? undefined)}`}
+          />
+          <StatisticCard
+            title="Remaining Slots"
+            loading={loadingProject}
+            value={`${projectData?.slots.remaining}/${projectData?.slots.total}`}
+            unit="Slots"
+          />
+          <StatisticCard
+            title="Budget Balance"
+            loading={loadingProject || loadingTokenSymbol}
+            value={`${projectData?.budget.remaining}/${projectData?.budget.total}`}
+            unit={tokenSymbol}
+          />
+        </div>
+      )}
 
       {/* Project Description and Action Panel */}
       <div className="w-2/3 flex flex-col lg:flex-row mx-auto gap-10 mb-10">
@@ -228,12 +200,20 @@ export default function Affiliate({ params }: { params: { projectId: string } })
           <h2 className="text-lg font-semibold text-gray-900">
             Earn {rewardText} for each successful referral
           </h2>
-          <p className="text-gray-600 pb-4">{isWhitelisted ? "Share your link with others and start earning!" : "Join the project to start referring others."}</p>
-          {isWhitelisted && 
+          <p className="text-gray-600 pb-4">
+            {projectData?.projectType === "DirectPayment" && isWhitelisted 
+              ? "Share your link with others and start earning!"
+              : projectData?.projectType === "EscrowPayment" && referralId
+              ? "Share your link with others and start earning!"
+              : "Join the project to start referring others."
+            }
+          </p>
+          {(projectData?.projectType === "DirectPayment" && isWhitelisted) ||
+           (projectData?.projectType === "EscrowPayment" && referralId) ? (
             <div className="flex bg-[#F3F4F6] rounded-md p-2 gap-3">
               <input
                 type="text"
-                value={REFERRAL_LINK}
+                value={referralLink}
                 readOnly
                 className="font-roboto text-sm bg-transparent outline-none w-full"
               />
@@ -245,7 +225,7 @@ export default function Affiliate({ params }: { params: { projectId: string } })
                 {buttonLabel}
               </button>
             </div>
-          }
+          ) : null}
           <div className="flex flex-col justify-stretch mt-4">
             <ConnectWallet
               theme={lightTheme({
@@ -256,61 +236,37 @@ export default function Affiliate({ params }: { params: { projectId: string } })
               modalSize={"compact"}
               modalTitleIconUrl={""}
               showThirdwebBranding={false}
-              // TODO: Google Formリンク表示機能を一時的に追加。
-              // そのため、リファラルIDを生成・取得する必要はない。
-              // - Reason: ギルド向け機能の開発のため。
-              // - Planned Reversion: 未定。
-              // - Date: 2024-05-15
-              // - Author: shungo0222
-              // - Issue: #305
-              // ===== UPDATE =====
-              // - Additional Modification: ウォレット接続時にホワイトリスト内に存在するのかを確認。
-              // - Reason for Update: ホワイトリストされたウォレット以外ははじくため。
-              // - Date of Update: 2024-05-17
-              // - Author: shungo0222
-              // - Issue: #315
-              // ===== BEGIN ORIGINAL CODE =====
-              // onConnect={async (wallet: WalletInstance) => {
-              //   try {
-              //     if (!projectData) {
-              //       // If project data is not yet loaded, wait for it to load
-              //       return;
-              //     }
-              //     const walletAddress = await wallet.getAddress();
-              //     const referralId = await joinProject(params.projectId, walletAddress);
-              //     console.log("Referral ID: ", referralId);
-              //     setReferralId(referralId);
-              //   } catch (error: any) {
-              //     console.error("Failed to join project: ", error);
-              //     toast.error(`Failed to join project: ${error.message}`);
-              //   }
-              // }}
-              // ===== END ORIGINAL CODE =====
-              // ===== BEGIN MODIFICATION =====
               onConnect={async (wallet: WalletInstance) => {
-                // Check the existence of the whitelist here
                 const walletAddress = await wallet.getAddress();
-                if (!projectData?.whitelistedAddresses[walletAddress]) {
-                  toast.error("Your wallet address is not whitelisted for this project.");
-                  disconnect();
-                  return;
+
+                if (projectData?.projectType === "DirectPayment") {
+                  // Check the existence of the whitelist here
+                  if (!projectData?.whitelistedAddresses[walletAddress]) {
+                    toast.error("Your wallet address is not whitelisted for this project.");
+                    disconnect();
+                    return;
+                  }
+                } else if (projectData?.projectType === "EscrowPayment") {
+                  try {
+                    if (!projectData) {
+                      // If project data is not yet loaded, wait for it to load
+                      return;
+                    }
+                    const referralId = await joinProject(params.projectId, walletAddress);
+                    console.log("Referral ID: ", referralId);
+                    setReferralId(referralId);
+                  } catch (error: any) {
+                    console.error("Failed to join project: ", error);
+                    toast.error(`Failed to join project: ${error.message}`);
+                  }
                 }
               }}
-              // ===== END MODIFICATION =====
             />
           </div>
         </div>
       </div>
 
-      {/*
-      TODO: コンバージョンダッシュボードを一時的に非表示する。
-      - Reason: コンバージョンに関してはGoogle Sheetsなどで管理できるため。
-      - Planned Reversion: 未定。
-      - Date: 2024-05-15
-      - Author: shungo0222
-      - Issue: #306
-      // ===== BEGIN ORIGINAL CODE =====
-      {address && 
+      {projectData?.projectType === "EscrowPayment" && address && 
         <>
           <div className="w-2/3 mx-auto grid grid-cols-1 lg:grid-cols-3 gap-5 mb-10">
             <StatisticCard
@@ -342,8 +298,6 @@ export default function Affiliate({ params }: { params: { projectId: string } })
           }
         </>
       }
-      // ===== END ORIGINAL CODE =====
-      */}
 
     </div>
   );
