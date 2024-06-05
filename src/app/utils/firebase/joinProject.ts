2cf8ef7672ba2954e6ec09ed13cde5a610989ed8
shungo0222 <kimura.shungo@gmail.com>
2024-06-05 17:44:48 +0900

    Refactor joinProject Function to Remove New User Data Saving Logic
    Related to #389

--- src/app/utils/firebase/joinProject.ts
+++ src/app/utils/firebase/joinProject.ts
@@ -5,21 +5,18 @@ import { toast } from "react-toastify";
 import { getExistingReferralId } from "./getExistingReferralId";
 import { createAndReturnNewReferralId } from "./createAndReturnNewReferralId";
 
-export async function joinProject(projectId: string, walletAddress: string): Promise<string> {
+export async function joinProject(
+  projectId: string, 
+  walletAddress: string,
+): Promise<string> {
   const userDocRef = doc(db, "users", walletAddress);
 
   try {
     const userDoc = await getDoc(userDocRef);
 
     if (!userDoc.exists()) {
-      const newUser: UserData = {
-        joinedProjectIds: [projectId],
-        createdAt: new Date(),
-        updatedAt: new Date()
-      };
-      await setDoc(userDocRef, newUser);
-      toast.success("Your account has been created successfully!");
-      return await createAndReturnNewReferralId(walletAddress, projectId);
+      toast.error("User document does not exist.");
+      throw new Error("User document does not exist.");
     } else {
       const userData = userDoc.data() as UserData;
       if (userData.joinedProjectIds.includes(projectId)) {
