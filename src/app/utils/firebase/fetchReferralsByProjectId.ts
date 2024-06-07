b2db7dc65aed7084299841f9603585e695c9c13c
shungo0222 <kimura.shungo@gmail.com>
2024-06-07 13:00:52 +0900

    Update fetchReferralsByProjectId Function to Include Username in ExtendedReferralData
    Related to #409

--- src/app/utils/firebase/fetchReferralsByProjectId.ts
+++ src/app/utils/firebase/fetchReferralsByProjectId.ts
@@ -1,29 +1,40 @@
-import { collection, query, where, getDocs, Timestamp } from "firebase/firestore";
+import { collection, query, where, getDocs, doc, getDoc, Timestamp } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 import { isValidReferralData } from "../validations";
-import { ReferralData } from "../../types";
+import { ReferralData, ExtendedReferralData, UserData } from "../../types";
 
-export async function fetchReferralsByProjectId(projectId: string): Promise<ReferralData[]> {
-  const referrals: ReferralData[] = [];
+export async function fetchReferralsByProjectId(projectId: string): Promise<ExtendedReferralData[]> {
+  const referrals: ExtendedReferralData[] = [];
   try {
     const q = query(collection(db, "referrals"), where("projectId", "==", projectId));
     const querySnapshot = await getDocs(q);
 
-    querySnapshot.forEach((doc) => {
-      const data = doc.data() as ReferralData & {
+    for (const docSnapshot of querySnapshot.docs) {
+      const data = docSnapshot.data() as ReferralData & {
         createdAt: Timestamp;
         lastConversionDate?: Timestamp | null;
       };
+
       if (isValidReferralData(data)) {
-        const referralData: ReferralData = {
+        const userDocRef = doc(db, "users", data.affiliateWallet);
+        const userDocSnapshot = await getDoc(userDocRef);
+
+        let username = "";
+        if (userDocSnapshot.exists()) {
+          const userData = userDocSnapshot.data() as UserData;
+          username = userData.username;
+        }
+
+        const referralData: ExtendedReferralData = {
           ...data,
-          id: doc.id,
+          id: docSnapshot.id,
           createdAt: data.createdAt.toDate(),
           lastConversionDate: data.lastConversionDate?.toDate() || null,
+          username,
         };
         referrals.push(referralData);
       }
-    });
+    }
 
     return referrals;
   } catch (error) {
