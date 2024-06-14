000e5c92d80ce3b79a54cd5f8e1dd56289e1a4f5
shungo0222 <kimura.shungo@gmail.com>
2024-06-14 17:29:11 +0900

    Make Status Bar Responsive in New Project Creation Screen
    Related to #501

--- src/app/components/createProject/statusbar/StatusStep.tsx
+++ src/app/components/createProject/statusbar/StatusStep.tsx
@@ -16,19 +16,21 @@ export const StatusStep: React.FC<StatusStepProps> = ({ number, step, currentSte
   }
 
   return (
-    <div className="flex flex-row items-center gap-2 md:gap-5">
+    <div className="flex flex-row items-center gap-3 md:gap-6">
       {number > 1 && 
-        <Image src="/status-arrow.png" height={20} width={20} alt="status-arrow.png" />
+        <div className="flex-shrink-0">
+          <Image src="/status-arrow.png" height={20} width={20} alt="status-arrow.png" />
+        </div>
       }
       {number < currentStep 
-        ? <div className="w-[40px] h-[40px] bg-blue-500 rounded-full flex justify-center items-center animate-checkmarkAppear">
+        ? <div className="w-[40px] h-[40px] min-w-[40px] min-h-[40px] bg-blue-500 rounded-full flex justify-center items-center animate-checkmarkAppear flex-shrink-0">
             <Image src="/checkmark.png" width={15} height={15} alt="checkmark.png" />
           </div>
-        : <div className={`border-2 ${number > currentStep ? "border-gray-500 text-gray-500" : "border-blue-500 text-blue-500 animate-bounce"} rounded-full w-[40px] h-[40px] flex justify-center items-center`}>
+        : <div className={`border-2 ${number > currentStep ? "border-gray-500 text-gray-500" : "border-blue-500 text-blue-500 animate-bounce"} rounded-full w-[40px] h-[40px] min-w-[40px] min-h-[40px] flex justify-center items-center flex-shrink-0`}>
             {number}
           </div>
       }
-      <p className={textStyle}>{step}</p>
+      <p className={`${textStyle} mr-5`}>{step}</p>
     </div>
   );
 }
