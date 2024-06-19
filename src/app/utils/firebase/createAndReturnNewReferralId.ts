bb652002afebab5895f41aabe9a8552a39f22c1a
shungo0222 <kimura.shungo@gmail.com>
2024-06-19 10:35:58 +0900

    Add click count tracking to referrals
    Related to #521

--- src/app/utils/firebase/createAndReturnNewReferralId.ts
+++ src/app/utils/firebase/createAndReturnNewReferralId.ts
@@ -10,7 +10,8 @@ export async function createAndReturnNewReferralId(walletAddress: string, projec
     createdAt: new Date(),
     conversions: 0,
     earnings: 0,
-    lastConversionDate: null
+    lastConversionDate: null,
+    clicks: 0,
   };
   const referralDocRef = await addDoc(collection(db, "referrals"), newReferral);
   toast.success("Your referral link has been generated successfully!");
