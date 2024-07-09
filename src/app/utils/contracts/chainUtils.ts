7cc1a84b9b817ef3b245913773bba59eaede857a
shungo0222 <kimura.shungo@gmail.com>
2024-07-09 15:21:02 +0900

    Implement EOA check and add loading indicator for adding team members
    Related to #677

--- src/app/utils/contracts/chainUtils.ts
+++ src/app/utils/contracts/chainUtils.ts
@@ -1,9 +1,21 @@
 import { Polygon, PolygonAmoyTestnet } from "@thirdweb-dev/chains";
+import { getProvider } from "./initializeSigner";
 
 export const getActiveChain = () => {
   if (process.env.NEXT_PUBLIC_ACTIVE_CHAIN === "Polygon") {
     return Polygon;
   } else {
     return PolygonAmoyTestnet;
+  }
+};
+
+export const isEOA = async (address: string): Promise<boolean> => {
+  const provider = getProvider();
+  try {
+    const code = await provider.getCode(address);
+    return code === "0x";
+  } catch (error) {
+    console.error("Error checking address:", error);
+    return false;
   }
 };
