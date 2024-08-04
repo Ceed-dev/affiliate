ac5d8982916bbb714c4c2f2176b8d54010810082
shungo0222 <kimura.shungo@gmail.com>
2024-08-04 16:53:35 +0900

    Update Delete Image Function for Specific File Deletion
    Related to #849

--- src/app/utils/firebase/deleteImageFromStorage.ts
+++ src/app/utils/firebase/deleteImageFromStorage.ts
@@ -4,21 +4,32 @@ import { ImageType } from "../../types";
 const storage = getStorage();
 
 /**
- * Deletes an image from Firebase storage based on the project ID and image type.
+ * Deletes images from Firebase storage based on the project ID and image type.
+ * If a file URL is provided, it deletes the specific file(s).
  * @param {string} projectId - The ID of the project.
- * @param {ImageType} type - The type of the image ("logo" or "cover").
+ * @param {ImageType} type - The type of the image ("logo", "cover", or "embeds").
+ * @param {string[]} [fileUrls] - The URLs of the specific files to delete.
  */
-export const deleteImageFromStorage = async (projectId: string, type: ImageType) => {
-  const storageRef = ref(storage, `projectImages/${projectId}/`);
+export const deleteImageFromStorage = async (projectId: string, type: ImageType, fileUrls?: string[]) => {
   try {
-    const listResult = await listAll(storageRef);
-    const fileToDelete = listResult.items.find(item => item.name.startsWith(type));
-
-    if (fileToDelete) {
-      await deleteObject(fileToDelete);
-      console.log(`Deleted: ${fileToDelete.fullPath}`);
-    } else {
-      console.log("No file matches the specified prefix.");
+    if (type === "embeds" && fileUrls) {
+      const deletePromises = fileUrls.map(url => {
+        const filePath = url.split("/o/")[1].split("?")[0].replace(/%2F/g, "/");
+        const fileRef = ref(storage, decodeURIComponent(filePath));
+        return deleteObject(fileRef);
+      });
+      await Promise.all(deletePromises);
+      console.log(`Deleted specified files in: projectImages/${projectId}/embeds/`);
+    } else if (type === "logo" || type === "cover") {
+      const storageRef = ref(storage, `projectImages/${projectId}/`);
+      const listResult = await listAll(storageRef);
+      const fileToDelete = listResult.items.find(item => item.name.startsWith(type));
+      if (fileToDelete) {
+        await deleteObject(fileToDelete);
+        console.log(`Deleted: ${fileToDelete.fullPath}`);
+      } else {
+        console.log("No file matches the specified prefix.");
+      }
     }
   } catch (error: any) {
     console.error("Error deleting file:", error);
