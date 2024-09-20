f9ab287cc9b4cb26024b24fd7c18888ebe0e8f5c
shungo0222 <kimura.shungo@gmail.com>
2024-09-20 20:00:42 +0900

    Refactor Admin Dashboard: Extract Token Summary Section into Separate Component
    Related to #1128

--- src/app/components/admin/index.ts
+++ src/app/components/admin/index.ts
@@ -1,9 +1,11 @@
 import { Header } from "./Header";
 import { AdminHeaderWithReloadButton } from "./AdminHeaderWithReloadButton";
 import { AdminTabs } from "./AdminTabs";
+import { TokenSummary } from "./TokenSummary";
 
 export {
   Header,
   AdminHeaderWithReloadButton,
   AdminTabs,
+  TokenSummary,
 }
