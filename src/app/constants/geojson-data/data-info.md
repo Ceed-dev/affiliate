925baf40e0e4a7b4441d1838dadbd475323bfb3c
shungo0222 <kimura.shungo@gmail.com>
2024-09-23 18:13:21 +0900

    Add Hong Kong boundary data to GeoJSON map
    Related to #1170

--- src/app/constants/geojson-data/data-info.md
+++ src/app/constants/geojson-data/data-info.md
@@ -3,6 +3,7 @@
 - Source[world-countries.json]: https://github.com/johan/world.geo.json/blob/master/countries.geo.json?short_path=afdfc39
 - Source[singapore.json]: https://github.com/yinshanyang/singapore/blob/master/maps/0-country.geojson
 - Source[faroe-islands.json]: https://cartographyvectors.com/map/908-faroe-islands-detailed-boundary
+- Source[hong-kong.json]: https://cartographyvectors.com/map/933-hong-kong-detailed-boundary
 - Last updated: September 23, 2024
 - Additional Notes: This data represents global country borders in GeoJSON format, containing the boundary information for each country.
 
@@ -11,3 +12,4 @@
 - **United States of America** -> **United States** (September 23, 2024 / Issue:#1162)
 - Added "singapore.json" file (September 23, 2024 / Issue:#1164)
 - Added "faroe-islands.json" file (September 23, 2024 / Issue:#1168)
+- Added "hong-kong.json" file (September 23, 2024 / Issue:#1170)
--- src/app/constants/geojson-data/index.ts
+++ src/app/constants/geojson-data/index.ts
@@ -7,16 +7,20 @@ import singaporeData from "./singapore.json";
 // Import Faroe Islands' boundary data from a separate GeoJSON file
 import faroeIslandsData from "./faroe-islands.json";
 
+// Import Hong Kong's boundary data from a separate GeoJSON file
+import hongKongData from "./hong-kong.json";
+
 /**
- * Combine the world countries GeoJSON data with Singapore's and Faroe Islands' GeoJSON data.
+ * Combine the world countries GeoJSON data with Singapore's, Faroe Islands', and Hong Kong's GeoJSON data.
  * This is necessary because the main 'world-countries.json' file doesn't 
- * include the boundary data for Singapore and the Faroe Islands, so we merge them manually.
+ * include the boundary data for Singapore, Faroe Islands, and Hong Kong, so we merge them manually.
  */
 export const combinedCountryData = {
   ...countryData, // Spread the existing properties of the world countries data
   features: [
     ...countryData.features, 
     ...singaporeData.features, 
-    ...faroeIslandsData.features // Merge the 'features' array with Singapore and Faroe Islands data
+    ...faroeIslandsData.features, 
+    ...hongKongData.features // Merge the 'features' array with Singapore, Faroe Islands, and Hong Kong data
   ]
 };
