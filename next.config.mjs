6fb34507147c5d9d67d483b2640e789325c326a8
shungo0222 <kimura.shungo@gmail.com>
2024-10-07 16:14:28 +0900

    Disable React Strict Mode to prevent API from being called twice during development
    Related to #1255

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
+  /** 
+   * Disabling React's Strict Mode.
+   * This prevents React from rendering components twice in development mode, which 
+   * can cause issues such as making API calls multiple times. It is safe to disable 
+   * in production, but React recommends using it in development to catch potential problems.
+   */
+  reactStrictMode: false,
 };
 
 export default nextConfig;
