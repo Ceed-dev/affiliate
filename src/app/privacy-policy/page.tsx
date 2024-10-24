3047cc5384d497e59124e3a156350a166962ba41
shungo0222 <kimura.shungo@gmail.com>
2024-10-24 10:35:09 +0900

    Fix a bug

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -42,7 +42,7 @@ export default function PrivacyPolicy() {
 
         <h2 className="text-2xl font-semibold mt-8 mb-4">Log Files</h2>
         <p className="mb-4">
-          Qube follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files includes internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
+          Qube follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services&apos; analytics. The information collected by log files includes internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users&apos; movement on the website, and gathering demographic information.
         </p>
 
         <h2 className="text-2xl font-semibold mt-8 mb-4">Advertising Partners Privacy Policies</h2>
@@ -50,7 +50,7 @@ export default function PrivacyPolicy() {
           You may consult this list to find the Privacy Policy for each of the advertising partners of Qube.
         </p>
         <p className="mb-4">
-          Third-party ad servers or ad networks use technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Qube, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+          Third-party ad servers or ad networks use technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Qube, which are sent directly to users&apos; browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
         </p>
         <p className="mb-4">
           Note that Qube has no access to or control over these cookies that are used by third-party advertisers.
@@ -58,10 +58,10 @@ export default function PrivacyPolicy() {
 
         <h2 className="text-2xl font-semibold mt-8 mb-4">Third Party Privacy Policies</h2>
         <p className="mb-4">
-          Qube's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
+          Qube&apos;s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
         </p>
         <p className="mb-4">
-          You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.
+          You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers&apos; respective websites.
         </p>
 
         <h2 className="text-2xl font-semibold mt-8 mb-4">CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
@@ -69,9 +69,9 @@ export default function PrivacyPolicy() {
           Under the CCPA, among other rights, California consumers have the right to:
         </p>
         <ul className="list-disc pl-5 mb-4 space-y-2">
-          <li>Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</li>
+          <li>Request that a business that collects a consumer&apos;s personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</li>
           <li>Request that a business delete any personal data about the consumer that a business has collected.</li>
-          <li>Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</li>
+          <li>Request that a business that sells a consumer&apos;s personal data, not sell the consumer&apos;s personal data.</li>
         </ul>
         <p className="mb-4">
           If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
@@ -93,7 +93,7 @@ export default function PrivacyPolicy() {
           If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
         </p>
 
-        <h2 className="text-2xl font-semibold mt-8 mb-4">Children's Information</h2>
+        <h2 className="text-2xl font-semibold mt-8 mb-4">Children&apos;s Information</h2>
         <p className="mb-4">
           Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
         </p>
