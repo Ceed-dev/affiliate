227f5b9c292c4bbc3bafaf569489e9148dfb881c
shungo0222 <kimura.shungo@gmail.com>
2024-10-25 12:09:26 +0900

    Refactor: Improve chain RPC URL structure and clean up unused chain definitions
    Related to #1418

--- src/app/constants/chains.ts
+++ src/app/constants/chains.ts
@@ -9,6 +9,47 @@ import {
   GesoVerse, GesotenVerseTestnet,
 } from "@thirdweb-dev/chains";
 
+// Production chains for mainnet environments
+export const productionChains: Chain[] = [
+  Polygon,
+  Arbitrum,
+  ArbitrumNova,
+  Avalanche,
+  Binance,
+  MchVerse,
+  HomeVerse,
+  GesoVerse,
+];
+
+// Test chains for testnet environments
+export const testChains: Chain[] = [
+  PolygonAmoyTestnet,
+  ArbitrumSepolia,
+  AvalancheFuji,
+  BinanceTestnet,
+  GesotenVerseTestnet,
+];
+
+// RPC URLs for different chains
+export const chainRpcUrls: { [chainId: number]: string } = {
+  137: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`, // Polygon Mainnet
+  80002: `https://polygon-amoy.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`, // Polygon Amoy Testnet
+  42161: `https://arb-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`, // Arbitrum Mainnet
+  42170: `https://arbnova-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`, // Arbitrum Nova
+  421614: `https://arb-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`, // Arbitrum Sepolia
+  43114: "https://api.avax.network/ext/bc/C/rpc", // Avalanche Mainnet
+  43113: "https://api.avax-test.network/ext/bc/C/rpc", // Avalanche Fuji Testnet
+  56: "https://bsc-dataseed.bnbchain.org", // Binance Mainnet
+  97: "https://data-seed-prebsc-1-s1.bnbchain.org:8545", // Binance Testnet
+  29548: "https://rpc.oasys.mycryptoheroes.net", // MchVerse Mainnet
+  19011: "https://rpc.mainnet.oasys.homeverse.games", // HomeVerse Mainnet
+  428: "https://rpc.verse.gesoten.com", // GesoVerse Mainnet
+  42801: "https://rpc.testnet.verse.gesoten.com", // GesotenVerse Testnet
+};
+
+// MchVerseTestnet and HomeVerseTestnet configurations are commented out.
+// Uncomment the following sections if you plan to use these chains in the future:
+
 // const MchVerseTestnet: Chain = {
 //   name: "MCH Verse Testnet",
 //   chain: "mchverse-testnet",
@@ -52,47 +93,3 @@ import {
 //     },
 //   ],
 // };
-
-export const productionChains: Chain[] = [
-  Polygon,
-  Arbitrum,
-  ArbitrumNova,
-  Avalanche,
-  Binance,
-  MchVerse,
-  HomeVerse,
-  GesoVerse,
-];
-
-export const testChains: Chain[] = [
-  PolygonAmoyTestnet,
-  ArbitrumSepolia,
-  AvalancheFuji,
-  BinanceTestnet,
-  // MchVerseTestnet,
-  // HomeVerseTestnet,
-  GesotenVerseTestnet,
-];
-
-export const chainRpcUrls: { [chainId: number]: string } = {
-  // 137: "https://polygon-mainnet.infura.io", // Polygon
-  137: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`, // Polygon
-  // 80002: "https://rpc-amoy.polygon.technology", // PolygonAmoyTestnet
-  80002: `https://polygon-amoy.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`, // PolygonAmoyTestnet
-  // 42161: "https://arb1.arbitrum.io/rpc", // Arbitrum
-  42161: `https://arb-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`, // Arbitrum
-  // 42170: "https://nova.arbitrum.io/rpc", // ArbitrumNova
-  42170: `https://arbnova-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`, // ArbitrumNova
-  // 421614: "https://sepolia-rollup.arbitrum.io/rpc", // ArbitrumSepolia
-  421614: `https://arb-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`, // ArbitrumSepolia
-  43114: "https://api.avax.network/ext/bc/C/rpc", // Avalanche
-  43113: "https://api.avax-test.network/ext/bc/C/rpc", // AvalancheFuji
-  56: "https://bsc-dataseed.bnbchain.org", // Binance
-  97: "https://data-seed-prebsc-1-s1.bnbchain.org:8545", // BinanceTestnet
-  29548: "https://rpc.oasys.mycryptoheroes.net", // MchVerse
-  // 420: "https://rpc.oasys.sand.mchdfgh.xyz", // MchVerseTestnet
-  19011: "https://rpc.mainnet.oasys.homeverse.games", // HomeVerse
-  // 40875: "https://rpc.testnet.oasys.homeverse.games", // HomeVerseTestnet
-  428: "https://rpc.verse.gesoten.com", // GesoVerse
-  42801: "https://rpc.testnet.verse.gesoten.com", // GesotenVerseTestnet
-};
